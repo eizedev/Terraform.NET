22e6684d668a9f07176d9b7c794d246eace41ceb
René <github@eize.dev>
2023-08-28 21:03:31 +0000

    added build.cmd

---
+++ build.cmd
@@ -0,0 +1,11 @@
+﻿@echo Off
+
+echo %~nx0: Building...
+dotnet build src/ --configuration Release --nologo || goto :error
+
+echo %~nx0: Testing...
+dotnet test src/ --configuration Release --no-build --nologo %1 %2 || goto :error
+
+goto :EOF
+:error
+exit /b %errorlevel%
